9fbe1e8e13684aa83464d74f0674be8dc52bd990
Dawid Glapiak <dg18@live.it>
2024-09-12 16:10:49 +0200

    bg lamp updated

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -113,7 +113,7 @@ const Team = () => {
         </motion.h2>
       </LampContainer>
       <motion.div
-        className="group mt-[-60px] mb-40 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8"
+        className=" group mt-[-60px] mb-40 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8"
         /* Use group for hover blur */
         variants={{
           hidden: { opacity: 0 },
