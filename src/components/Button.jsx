055a521f8dc741dd271fbcd8fb03da2eec272a57
Dawid Glapiak <dg18@live.it>
2024-09-19 15:06:44 +0200

    changed hover on a button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,13 +7,18 @@ const Button = ({ styles }) => {
 
   return (
     <Link to="/contact">
-      {" "}
-      {/* Wrap the button with the Link component */}
       <button
         type="button"
-        className={`min-w-[90vw] ss:min-w-[15vw] mt-6 py-4 px-4 font-poppins font-medium text-[18px] border-b-2 border-r-2 border-yellow-400 text-white bg-black-gradient rounded-[10px] outline-none`}
+        className={`my-10 relative min-w-[60vw] ss:min-w-[15vw] mt-6 py-4 px-4 font-poppins font-medium text-[18px]   text-white bg-black-gradient rounded-[10px] overflow-hidden group hover:text-zinc-200 transition-all duration-300`}
       >
-        {t("button.reserve")} {/* Use translation for the button text */}
+        {/* Effetto Glow quando si passa sopra */}
+        <span className="absolute inset-0 bg-black opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+
+        {/* Contenuto del bottone */}
+        <span className="relative z-10">{t("button.reserve")}</span>
+
+        {/* Linea di bordi Glow */}
+        <span className="absolute inset-0 rounded-[10px] border-b-2 border-r-2 border-yellow-400 group-hover:border-yellow-500  transition-all duration-300"></span>
       </button>
     </Link>
   );
